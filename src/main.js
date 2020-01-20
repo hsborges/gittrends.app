66ac96a1a741bdd53e72ff83b730158fa3fb2a3f
Hudson SIlva Borges <hudsonsilbor@gmail.com>
2020-01-20 10:36:05 -0400

    add google analytics

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import VueGtag from "vue-gtag";
 
 import "./assets/styles/index.css";
 
 Vue.config.productionTip = false;
+
+Vue.use(VueGtag, { config: { id: process.env.VUE_APP_GA_ID } }, router);
 
 new Vue({
   router,
